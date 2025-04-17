4fa0fb8853dc157685dccb4c2278f1509ed0d04f
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2025-04-18 02:07:53 +0600

    added completetoggle

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,20 +1,36 @@
 import { Pencil, Trash } from "lucide-react";
 import { Button } from "../ui/button";
 import { useAppDispatch } from "@/redux/hooks";
-import { removeTodo } from "@/redux/features/todoSlice";
+import { removeTodo, toggleComplete } from "@/redux/features/todoSlice";
 type TTodoCardProps = {
   id: string;
   title: string;
   description: string;
+  isCompleted?: boolean;
 };
-const TodoCard = ({ id, title, description }: TTodoCardProps) => {
+const TodoCard = ({ id, title, description, isCompleted }: TTodoCardProps) => {
   const dispatch = useAppDispatch();
 
+  const toggleCompleteState = () => {
+    console.log("complete");
+    dispatch(toggleComplete(id));
+  };
   return (
     <div className="flex justify-between items-center p-3 bg-white rounded-sm border">
-      <input type="checkbox" name="" id="" />
+      <input
+        onChange={toggleCompleteState}
+        type="checkbox"
+        name="complete"
+        id=""
+      />
       <p className="font-medium">{title}</p>
-      <p>Time</p>
+      <div>
+        {isCompleted ? (
+          <p className="text-green-800">Done</p>
+        ) : (
+          <p className="text-red-500">Pending</p>
+        )}
+      </div>
       <p>{description}</p>
       <div className="space-x-3">
         <Button onClick={() => dispatch(removeTodo(id))} className="bg-red-600">
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -22,7 +22,20 @@ export const todoSlice = createSlice({
     removeTodo: (state, action: PayloadAction<string>) => {
       state.todos = state.todos.filter((item) => item.id !== action.payload);
     },
+    toggleComplete: (state, action: PayloadAction<string>) => {
+      const task = state.todos.find((item) => item.id === action.payload);
+      // toggling isCompletede value
+      task!.isCompleted = !task?.isCompleted;
+      //sorting
+      state.todos = state.todos.sort((a, b) => {
+        return Number(a.isCompleted) - Number(b.isCompleted);
+        /*true becomes 1
+        false or undefined becomes 0 when converted with Number()
+        So uncompleted (or undefined) todos will come first, and completed ones will be sorted to the bottom.
+        */
+      });
+    },
   },
 });
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, toggleComplete } = todoSlice.actions;
 export default todoSlice.reducer;
